7b82e72a3c6c4fec405f981b55f441f344c0bc45
tehlordvortex <tehlordvortex@outlook.com>
2018-10-23 20:25:57 +0100

    skip authentication

--- signup.php
+++ signup.php
@@ -1,60 +1,61 @@
 <?php
-include('database.php'); //To import the Database connection file
-if ($Mysqli_database) {
-    if(isset($_POST['signup'])) { //for signing up
-       // if($_POST['signup'] == 'submit-signup') 
-        {
-         if($_POST['Password'] == $_POST['Confirm_Password']) {
-          $sqlsignup = "INSERT INTO devminds_members (EMAIL,PASSWORD) 
-          VALUES ('".$_POST['Email']."','".$_POST['Password']."');";
-          if(mysqli_query($Mysqli_database,$sqlsignup)) {
-              //include('index.php');
-              echo "<script>
-                   alert('Account Created Successfully, You Need to login to Begin');
-                  </script>";
-                header("Location:login.html");
+header('Location: profile.php');
+// include('database.php'); //To import the Database connection file
+// if ($Mysqli_database) {
+//     if(isset($_POST['signup'])) { //for signing up
+//        // if($_POST['signup'] == 'submit-signup') 
+//         {
+//          if($_POST['Password'] == $_POST['Confirm_Password']) {
+//           $sqlsignup = "INSERT INTO devminds_members (EMAIL,PASSWORD) 
+//           VALUES ('".$_POST['Email']."','".$_POST['Password']."');";
+//           if(mysqli_query($Mysqli_database,$sqlsignup)) {
+//               //include('index.php');
+//               echo "<script>
+//                    alert('Account Created Successfully, You Need to login to Begin');
+//                   </script>";
+//                 header("Location:login.html");
               
-          } else {
-              echo 'error while signing in '.mysqli_error();
-          }
-        } else {
+//           } else {
+//               echo 'error while signing in '.mysqli_error();
+//           }
+//         } else {
             
-            echo "<script>
-                   alert('Password and Confirm Password did not math');
-                  </script>";
-                  include("signup.html");
+//             echo "<script>
+//                    alert('Password and Confirm Password did not math');
+//                   </script>";
+//                   include("signup.html");
                   
-        }
-        }
-    } elseif(isset($_POST['login'])) { //for logging in
+//         }
+//         }
+//     } elseif(isset($_POST['login'])) { //for logging in
         
-       //if($_POST['LOGIN'] == 'SIGNIN') 
-       {
-        #echo $_POST['USERNAME'];
-        $Email = mysqli_escape_string($Mysqli_database,$_POST['Email']);
-        $Password = mysqli_escape_string($Mysqli_database,$_POST['Password']);
-        //echo $Email,$Password;
-        $checklogin = "SELECT * FROM devminds_members WHERE (EMAIL = '$Email' AND PASSWORD = '$Password');";
-        $mysqli = mysqli_query($Mysqli_database,$checklogin);
-        //$userdetails = mysqli_fetch_assoc($mysqli);
-        if($mysqli AND mysqli_num_rows($mysqli) == 1)  { // return true if queried true and queried rows is only one.
+//        //if($_POST['LOGIN'] == 'SIGNIN') 
+//        {
+//         #echo $_POST['USERNAME'];
+//         $Email = mysqli_escape_string($Mysqli_database,$_POST['Email']);
+//         $Password = mysqli_escape_string($Mysqli_database,$_POST['Password']);
+//         //echo $Email,$Password;
+//         $checklogin = "SELECT * FROM devminds_members WHERE (EMAIL = '$Email' AND PASSWORD = '$Password');";
+//         $mysqli = mysqli_query($Mysqli_database,$checklogin);
+//         //$userdetails = mysqli_fetch_assoc($mysqli);
+//         if($mysqli AND mysqli_num_rows($mysqli) == 1)  { // return true if queried true and queried rows is only one.
     
-            session_start(); // TO START A SESSION FOR LOGGED IN USER
-            $_SESSION['Email'] = $Email;
-            header("Location:index.html");
+//             session_start(); // TO START A SESSION FOR LOGGED IN USER
+//             $_SESSION['Email'] = $Email;
+//             header("Location:index.html");
             
-        } else {
-            echo "<script>
-                   alert('Incorrect Email or Password');
-                  </script>";
-                echo incude("login.html");      
-                }
-       }
-    } else {
-        header("Location:signup.html");
-    }
-} else {
-//include('critedbottominfo.php');
-echo 'Error';
-}
+//         } else {
+//             echo "<script>
+//                    alert('Incorrect Email or Password');
+//                   </script>";
+//                 echo incude("login.html");      
+//                 }
+//        }
+//     } else {
+//         header("Location:signup.html");
+//     }
+// } else {
+// //include('critedbottominfo.php');
+// echo 'Error';
+// }
 ?>
